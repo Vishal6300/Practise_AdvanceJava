369b84d852da1d4a6a11d52e6c3af742c2b8a80f
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-10 11:58:31 +0530

    added some changes

--- Sprint2_day4/src/class1/FileDemo.java
+++ Sprint2_day4/src/class1/FileDemo.java
@@ -15,7 +15,9 @@ public class FileDemo {
 		System.out.println("FIle name is "+ file.getName());
 		System.out.println("Path of file is "+file.getPath());
 		System.out.println("Absolute path of file is "+ file.getAbsolutePath());
-		
+		System.out.println("A.txt file is "+ file.isFile());
+		System.out.println("A.txt is directory? "+ file.isDirectory());
+		System.out.println("A.txt file length is " + file.length());
 	}
 
 }
