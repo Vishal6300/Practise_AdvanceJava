d8e4ebc17df6f90b348896a84b86a391de687b45
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-10 12:03:35 +0530

    added some changes

--- Sprint2_day4/src/class1/FileDemo.java
+++ Sprint2_day4/src/class1/FileDemo.java
@@ -1,6 +1,7 @@
 package class1;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileDemo {
 
@@ -18,6 +19,22 @@ public class FileDemo {
 		System.out.println("A.txt file is "+ file.isFile());
 		System.out.println("A.txt is directory? "+ file.isDirectory());
 		System.out.println("A.txt file length is " + file.length());
+	
+	File anotherFile= new File("B.txt");
+	
+	if(!anotherFile.exists()) {
+		System.out.println("No file found named B.txt");
+		try{
+			System.out.println("Creating the file");
+			anotherFile.createNewFile();
+			System.out.println("FIle is created");
+		}catch(IOException ex) {
+			System.out.println("Not created");
+		}
+		
+	}
+	
+	
 	}
 
 }
