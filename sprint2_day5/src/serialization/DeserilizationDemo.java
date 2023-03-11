b375e0deaf01938d66928485e89b5c93b44ad39d
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-11 13:13:45 +0530

    added some changes

--- sprint2_day5/src/serialization/DeserilizationDemo.java
+++ sprint2_day5/src/serialization/DeserilizationDemo.java
@@ -1,10 +1,19 @@
 package serialization;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 public class DeserilizationDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		ObjectInput oi= new ObjectInputStream(new FileInputStream("human.ser"));
+		Human human= (Human)oi.readObject();
+		oi.close();
+		
+		System.out.println(human);
+		
 	}
 
 }
