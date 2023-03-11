61b52c51be44c67e3ac1f84192683888046ad897
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-11 12:12:20 +0530

    added FileInput Stream class of Java

---
+++ Sprint2_day4/src/class2/PrimitiveValueReadDemo.java
@@ -0,0 +1,22 @@
+package class2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class PrimitiveValueReadDemo {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc= new Scanner(new FileInputStream("student.txt"));
+		System.out.println("give details");	
+		String name= sc.next();
+			int std= sc.nextInt();
+			double percentage= sc.nextDouble();
+			boolean handicapped= sc.nextBoolean();
+			String cityState= sc.nextLine();
+			System.out.println("Name: "+ name +" Standard : "+ std + " Percentage: "+ percentage+" handcapped: "+ handicapped+" city & state : "+ cityState);
+			
+			sc.close();
+	}
+
+}
