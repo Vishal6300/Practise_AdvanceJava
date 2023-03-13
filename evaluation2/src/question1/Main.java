3c396e0cd6ebed7234b435e8fb173910b2128f4b
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-13 15:00:01 +0530

    added c2 evaluation folder 13th march

---
+++ evaluation2/src/question1/Main.java
@@ -0,0 +1,18 @@
+package question1;
+
+class Virat extends Thread{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
+	
+}
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
