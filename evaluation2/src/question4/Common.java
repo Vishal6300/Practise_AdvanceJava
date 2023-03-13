e586cd187cc0b1451bd824b28f7d5e20bf4aeee7
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-13 16:25:29 +0530

    added 3rd question

--- evaluation2/src/question4/Common.java
+++ evaluation2/src/question4/Common.java
@@ -1,5 +1,58 @@
 package question4;
 
 public class Common {
+	
+	 int value;
+	 boolean flag =true;
+	 
+	 public synchronized void producer(int i) {
+		 
+		 
+		 if(flag = true) {
+			 
+			 value =i;
+			 
+			 System.out.println("Producer Produced "+i);
+			 
+			 flag = false;
+			 
+			 this.notify();
+			 
+			 try {
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			 
+			 
+		 }
+	 }
+	 
+	 public  synchronized int  consumer() {
+		 
+		 if(flag == true) {
+			 
+			 try {
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			 
+			 
+		 }
+		 
+		 flag = true;
+		 
+		 this.notify();
+		 
+		 return value;
+		 
+		 
+	 }
+		
+		
+		
 
-}
+	}
