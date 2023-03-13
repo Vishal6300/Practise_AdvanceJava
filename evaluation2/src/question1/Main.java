8ad6547555f571d10874ec8ddd84478f135bd21b
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-13 15:11:56 +0530

    added 1st question

--- evaluation2/src/question1/Main.java
+++ evaluation2/src/question1/Main.java
@@ -8,18 +8,33 @@ class Virat extends Thread{
 		// TODO Auto-generated method stub
 		for(int i=1;i<=20;i++) {
 			sum+=i;
+			System.out.println(Thread.currentThread().getName()+" "+ sum);
 		}
-		System.out.println(Thread.currentThread().getName()+ sum);
+		
 	}
 	
 }
 class Dhoni extends Thread{
 	public void run() {
 		for(int i=1;i<=20;i++) {
-			System.out.println(Thread.currentThread().getName()+ i);
+			System.out.println(Thread.currentThread().getName()+" " +i);
 		}
 		
 	}
+}
+class Rohit extends Thread{
+	int product=1;
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=20;i++) {
+			product=product*i;
+			System.out.println(Thread.currentThread().getName()+ " "+ product);
+		}
+		
+	}
+	
 }
 
 
