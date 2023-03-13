1bbb6cf52e33ec5a05f770e3a6b5da032aeae4ac
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-14 04:45:11 +0530

    added some changes

--- sprint3_Day1/src/class1/Main.java
+++ sprint3_Day1/src/class1/Main.java
@@ -1,9 +1,20 @@
 package class1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		Path p = Paths.get("d://abc//a1.txt"); // here d:/abc folder must be there,otherwise we get an exception
+		if(Files.exists(p)) {
+		System.out.println("File is aready exist");
+		}else {
+		
+		}
 
 	}
 
