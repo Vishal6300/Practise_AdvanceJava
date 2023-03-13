e742bb07340934593923102224d4f34278a9f2b1
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-13 15:06:31 +0530

    added 1st question

--- evaluation2/src/question1/Main.java
+++ evaluation2/src/question1/Main.java
@@ -1,13 +1,25 @@
 package question1;
 
 class Virat extends Thread{
-
+		
+	int sum=0;
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		super.run();
+		for(int i=1;i<=20;i++) {
+			sum+=i;
+		}
+		System.out.println(Thread.currentThread().getName()+ sum);
 	}
 	
+}
+class Dhoni extends Thread{
+	public void run() {
+		for(int i=1;i<=20;i++) {
+			System.out.println(Thread.currentThread().getName()+ i);
+		}
+		
+	}
 }
 
 
