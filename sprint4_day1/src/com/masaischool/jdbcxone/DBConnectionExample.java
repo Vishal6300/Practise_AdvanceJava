51e941c3af3edc9f5a6a4f5768e964672f48087f
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-21 12:24:47 +0530

    added some more classes

--- sprint4_day1/src/com/masaischool/jdbcxone/DBConnectionExample.java
+++ sprint4_day1/src/com/masaischool/jdbcxone/DBConnectionExample.java
@@ -1,17 +1,20 @@
 package com.masaischool.jdbcxone;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnectionExample {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
 		Connection conn= DriverManager.getConnection("jdbc:mysql://localhost/sb101_feb","root","root");
 		
-		System.out.println(conn);
+		System.out.println("Connection established successfully");
+		
+		conn.close();
 
 	}
 
--- sprint4_day1/src/module-info.java
+++ sprint4_day1/src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module sprint4_day1 {
+	requires java.sql;
 }
