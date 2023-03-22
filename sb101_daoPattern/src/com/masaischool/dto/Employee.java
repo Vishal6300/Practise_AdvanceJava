007509b28fd1f01bef978e70b482832c5369e830
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-22 13:57:01 +0530

    added some classes

--- sb101_daoPattern/src/com/masaischool/dto/Employee.java
+++ sb101_daoPattern/src/com/masaischool/dto/Employee.java
@@ -1,5 +1,12 @@
 package com.masaischool.dto;
 
-public class Employee {
+import java.time.LocalDate;
 
+public class Employee {
+	private String empid;
+	private String name;
+	private double salary;
+	private LocalDate date;
+	
+	
 }
