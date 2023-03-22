6464353357ec2e9ad3c6b71ba0a1e2d92f009fb6
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-22 13:58:20 +0530

    added some classes

--- sb101_daoPattern/src/com/masaischool/dto/Employee.java
+++ sb101_daoPattern/src/com/masaischool/dto/Employee.java
@@ -7,6 +7,37 @@ public class Employee {
 	private String name;
 	private double salary;
 	private LocalDate date;
+	public String getEmpid() {
+		return empid;
+	}
+	public void setEmpid(String empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public Employee(String empid, String name, double salary, LocalDate date) {
+		super();
+		this.empid = empid;
+		this.name = name;
+		this.salary = salary;
+		this.date = date;
+	}
 	
 	
 }
