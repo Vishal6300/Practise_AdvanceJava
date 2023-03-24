4491847a9d6b98e21181407f7ee40c94192dd7bc
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-24 09:54:48 +0530

    added some classes

--- sprint4_MobileApp/src/com/mobileDto/Mobile.java
+++ sprint4_MobileApp/src/com/mobileDto/Mobile.java
@@ -1,5 +1,16 @@
 package com.mobileDto;
 
-public interface Mobile {
+import java.time.LocalDate;
 
+public interface Mobile {
+	public int getId();
+	public void setId(int id);
+	public String getModel_no();
+	public void setModel_no(String model_no);
+	public String getCompany();
+	public void setCompany(String company);
+	public int getPrice();
+	public void setPrice(int price);
+	public LocalDate getMFGdate();
+	public void setMFGdate(LocalDate mFGdate);
 }
