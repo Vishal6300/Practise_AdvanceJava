ff6fdb72156a7a5f5801c76780ea42c69ce1e8b3
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-26 22:43:58 +0530

    added some classes

--- sb101_b24_dao_example_1/sb101_b24_dao_example_1/src/com/masaischool/ui/UIMain.java
+++ sb101_b24_dao_example_1/sb101_b24_dao_example_1/src/com/masaischool/ui/UIMain.java
@@ -14,113 +14,116 @@ import com.masaischool.exception.SomethingWentWrongException;
 
 public class UIMain {
 	static void addEmployeeUI(Scanner sc) {
-		//take input employee details
-		System.out.print("Enter employee id ");
-		String eid = sc.next();
-		
+		// take input employee details
+
+		// System.out.print("Enter employee id ");
+		// String eid = sc.next();
+
 		System.out.print("Enter employee name ");
 		String name = sc.next();
-		
+
 		System.out.print("Enter salary ");
 		double salary = sc.nextDouble();
-		
+
 		System.out.print("Enter joining date ");
 		LocalDate joiningDate = LocalDate.parse(sc.next());
-		
-		//Create an object of DTO
+
+		// Create an object of DTO
 		Employee employee = new EmployeeImpl(eid, name, salary, joiningDate);
-		
+
 		EmployeeDAO empDAO = new EmployeeDAOImpl();
-		
+
 		try {
-			empDAO.addEmployee(employee);	
+			empDAO.addEmployee(employee);
 			System.out.println("Employee added successfully");
-		}catch(SomethingWentWrongException ex) {
+		} catch (SomethingWentWrongException ex) {
 			System.out.println(ex);
 		}
-		//call the method of dao layer to add employee in database and get status
-		//if successful then "Employee added successfully"
-		//if failed then provide suitable message
+		// call the method of dao layer to add employee in database and get status
+		// if successful then "Employee added successfully"
+		// if failed then provide suitable message
 	}
-	
+
 	static void updateEmployeeUI(Scanner sc) {
-		//take input employee details
+		// take input employee details
 		System.out.print("Enter employee id ");
 		String eid = sc.next();
-		
+
 		System.out.print("Enter employee name ");
 		String name = sc.next();
-		
+
 		System.out.print("Enter salary ");
 		double salary = sc.nextDouble();
-		
+
 		System.out.print("Enter joining date ");
 		LocalDate joiningDate = LocalDate.parse(sc.next());
-		
-		//Create an object of DTO
+
+		// Create an object of DTO
 		Employee employee = new EmployeeImpl(eid, name, salary, joiningDate);
-		
-		//take an object of DAO
+
+		// take an object of DAO
 		EmployeeDAO empDAO = new EmployeeDAOImpl();
 		try {
-			//call the update employee method
+			// call the update employee method
 			empDAO.updateEmployee(employee);
-			//print success message
+			// print success message
 			System.out.println("Employee updated successfully");
-		}catch(SomethingWentWrongException ex) {
-			//print error message if error if there
+		} catch (SomethingWentWrongException ex) {
+			// print error message if error if there
 			System.out.println(ex);
 		}
 	}
-	
+
 	static void deleteEmployeeUI(Scanner sc) {
 		System.out.print("Enter employee id ");
 		String eid = sc.next();
-		
-		//take an object of DAO
+
+		// take an object of DAO
 		EmployeeDAO empDAO = new EmployeeDAOImpl();
-		
+
 		try {
-			//call the update employee method
+			// call the update employee method
 			empDAO.deleteEmployee(eid);
-			//print success message
+			// print success message
 			System.out.println("Employee deleted successfully");
-		}catch(SomethingWentWrongException ex) {
-			//print error message if error if there
+		} catch (SomethingWentWrongException ex) {
+			// print error message if error if there
 			System.out.println(ex);
 		}
 	}
-	
+
 	static void viewEmployee() {
-		//take an object of DAO
+		// take an object of DAO
 		EmployeeDAO empDAO = new EmployeeDAOImpl();
 		try {
 			List<Employee> empList = empDAO.getEmployeeList();
-			Consumer<Employee> con = emp -> System.out.println("Employee Id " + emp.getEmpId() + " Name " + emp.getName() 
-			+ " Salary " + emp.getSalary() + " Joining Date " + emp.getJoiningDate());
+			Consumer<Employee> con = emp -> System.out
+					.println("Employee Id " + emp.getEmpId() + " Name " + emp.getName()
+							+ " Salary " + emp.getSalary() + " Joining Date " + emp.getJoiningDate());
 			empList.forEach(con);
-		}catch(SomethingWentWrongException | NoRecordFoundException ex) {
+		} catch (SomethingWentWrongException | NoRecordFoundException ex) {
 			System.out.println(ex.getMessage());
 		}
 	}
-	
+
 	static void searchEmployeeByJoiningDateRange(Scanner sc) {
 		System.out.print("Enter start date ");
 		LocalDate startDate = LocalDate.parse(sc.next());
 		System.out.print("Enter end date ");
 		LocalDate endDate = LocalDate.parse(sc.next());
-		//take an object of DAO
+		// take an object of DAO
 		EmployeeDAO empDAO = new EmployeeDAOImpl();
 		try {
 			List<Employee> empList = empDAO.searchEmployeeByJoiningDateRange(startDate, endDate);
-			Consumer<Employee> con = emp -> System.out.println("Employee Id " + emp.getEmpId() + " Name " + emp.getName() 
-			+ " Salary " + emp.getSalary() + " Joining Date " + emp.getJoiningDate());
+			Consumer<Employee> con = emp -> System.out
+					.println("Employee Id " + emp.getEmpId() + " Name " + emp.getName()
+							+ " Salary " + emp.getSalary() + " Joining Date " + emp.getJoiningDate());
 			empList.forEach(con);
-		}catch(SomethingWentWrongException | NoRecordFoundException ex) {
+		} catch (SomethingWentWrongException | NoRecordFoundException ex) {
 			System.out.println(ex.getMessage());
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int choice = 0;
@@ -133,8 +136,8 @@ public class UIMain {
 			System.out.println("5. Search Employee By Joining Date Range");
 			System.out.print("Enter Selection ");
 			choice = sc.nextInt();
-			
-			switch(choice) {
+
+			switch (choice) {
 				case 1:
 					addEmployeeUI(sc);
 					break;
@@ -156,8 +159,8 @@ public class UIMain {
 				default:
 					System.out.println("Invalid Selection please try again later");
 			}
-			
-		}while(choice != 0);
+
+		} while (choice != 0);
 		sc.close();
 	}
 }
